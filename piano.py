122a87a4f27c2a04d854bd65b975a92bb3291d0b
craigv <vear2ev2.co.uk>
2021-10-20 15:28:47 +0100

    adding jazz piano algo

--- piano.py
+++ piano.py
@@ -26,8 +26,8 @@
 # from mingus.core import notes, chords
 from mingus.containers import *
 from mingus.midi import fluidsynth
-# import sys
-from random import randrange
+from time import sleep
+from random import random, randrange, getrandbits
 
 
 class Piano:
@@ -36,277 +36,121 @@ class Piano:
         self.OCTAVES = 5  # number of octaves to show
         self.LOWEST = 2  # lowest octave to show
         self.FADEOUT = 0.25  # coloration fadeout time (1 tick = 0.001)
-        # self.WHITE_KEY = 0
-        # self.BLACK_KEY = 1
-        # self.WHITE_KEYS = [
-        #     "C",
-        #     "D",
-        #     "E",
-        #     "F",
-        #     "G",
-        #     "A",
-        #     "B",
-        # ]
-        # self.BLACK_KEYS = ["C#", "D#", "F#", "G#", "A#"]
 
-        self.dmin7 = [("D", 50), ("F", 70), ("A", 30), ("C", 60)]
-        self.g9 = [("G", 50), ("B", 70), ("D", 30), ("F", 60), ("A", 60)]
-        self.cM7 = [("C", 50), ("E", 70), ("G", 30), ("B", 60)]
+        self.note_list = ["A", "B", "C", "D", "E", "F", "G", "A"]
 
-
-        # todo - matrix with jazz chords and whole note on as dictionaries and weightings
-
+        # set up harmonic matrix with weighting adding to 100%
+        self.dmin7 = [(3, 20), (5, 40), (0, 10), (2, 30)]
+        self.g9 = [(6, 15), (1, 35), (3, 5), (5, 20), (0, 25)]
+        self.cM7 = [(2, 20), (4, 40), (6, 10), (1, 30)]
 
         self.octave = 4
         self.channel = 8
 
-# def load_img(name):
-#     """Load image and return an image object"""
-#
-#     fullname = name
-#     try:
-#         image = pygame.image.load(fullname)
-#         if image.get_alpha() is None:
-#             image = image.convert()
-#         else:
-#             image = image.convert_alpha()
-#     except pygame.error as message:
-#         print("Error: couldn't load image: ", fullname)
-#         raise SystemExit(message)
-#     return (image, image.get_rect())
-
-# if not
+        # start fluidsynth
         fluidsynth.init(SF2)
-#     print("Couldn't load soundfont", SF2)
-#     sys.exit(1)
-#
-# pygame.init()
-# pygame.font.init()
-# font = pygame.font.SysFont("monospace", 12)
-# screen = pygame.display.set_mode((640, 480))
-#
-# (key_graphic, kgrect) = load_img("media/keys.png")
-# (width, height) = (kgrect.width, kgrect.height)
-# white_key_width = width / 7
+        # self.played_note = "C"
 
-# Reset display to wrap around the keyboard image
-
-# pygame.display.set_mode((OCTAVES * width, height + 20))
-# pygame.display.set_caption("mingus piano")
-
-
-# pressed is a surface that is used to show where a key has been pressed
-#
-# pressed = pygame.Surface((white_key_width, height))
-# pressed.fill((0, 230, 0))
-
-# text is the surface displaying the determined chord
-
-# text = pygame.Surface((width * OCTAVES, 20))
-# text.fill((255, 255, 255))
-        playing_w = []  # white keys being played right now
-        playing_b = []  # black keys being played right now
-        quit = False
-        tick = 0.0
+        # start someform of clock for chord sequence
+        self.tick = 0.0
 
 
     def play_note(self, note):
         """play_note determines the coordinates of a note on the keyboard image
         and sends a request to play the note to the fluidsynth server"""
 
-        # global text
-        # octave_offset = (note.octave - LOWEST) * width
-        # if note.name in WHITE_KEYS:
-        #
-        #     # Getting the x coordinate of a white key can be done automatically
-        #
-        #     w = WHITE_KEYS.index(note.name) * white_key_width
-        #     w = w + octave_offset
-        #
-        #     # Add a list containing the x coordinate, the tick at the current time
-        #     # and of course the note itself to playing_w
-        #
-        #     playing_w.append([w, tick, note])
-        # else:
-        #
-        #     # For black keys I hard coded the x coordinates. It's ugly.
-        #
-        #     i = BLACK_KEYS.index(note.name)
-        #     if i == 0:
-        #         w = 18
-        #     elif i == 1:
-        #         w = 58
-        #     elif i == 2:
-        #         w = 115
-        #     elif i == 3:
-        #         w = 151
-        #     else:
-        #         w = 187
-        #     w = w + octave_offset
-        #     playing_b.append([w, tick, note])
-
-        # To find out what sort of chord is being played we have to look at both the
-        # white and black keys, obviously:
-
-        # notes = playing_w + playing_b
-        # notes.sort()
-        # notenames = []
-        # for n in notes:
-        #     notenames.append(n[2].name)
-        #
-        # # Determine the chord
-        #
-        # det = chords.determine(notenames)
-        # if det != []:
-        #     det = det[0]
-        # else:
-        #     det = ""
-
-        # And render it onto the text surface
-
-        # t = font.render(det, 2, (0, 0, 0))
-        # text.fill((255, 255, 255))
-        # text.blit(t, (0, 0))
-
-        # Play the note
-
         fluidsynth.play_Note(note, self.channel, 100)
 
-
-# while not quit:
-
-    # Blit the picture of one octave OCTAVES times.
-    #
-    # for x in range(OCTAVES):
-    #     screen.blit(key_graphic, (x * width, 0))
-    #
-    # # Blit the text surface
-    #
-    # screen.blit(text, (0, height))
-    #
-    # # Check all the white keys
-
-        # for note in playing_w:
-        #     diff = tick - note[1]
-        #
-        #     # If a is past its prime, remove it, otherwise blit the pressed surface
-        #     # with a 'cool' fading effect.
-        #
-        #     if diff > FADEOUT:
-        #         fluidsynth.stop_Note(note[2], channel)
-        #         playing_w.remove(note)
-        #     # else:
-        #     #     pressed.fill((0, ((FADEOUT - diff) / FADEOUT) * 255, 124))
-        #     #     screen.blit(pressed, (note[0], 0), None, pygame.BLEND_SUB)
-        #
-        # # Now check all the black keys. This redundancy could have been prevented,
-        # # but it isn't any less clear like this
-        #
-        # for note in playing_b:
-        #     diff = tick - note[1]
-        #
-        #     # Instead of SUB we ADD this time, and change the coloration
-        #
-        #     if diff > FADEOUT:
-        #         fluidsynth.stop_Note(note[2], channel)
-        #         playing_b.remove(note)
-        # else:
-        #     pressed.fill((((FADEOUT - diff) / FADEOUT) * 125, 0, 125))
-        #     screen.blit(pressed, (note[0], 1), (0, 0, 19, 68), pygame.BLEND_ADD)
-
-    # Check for keypresses
-
     def which_note(self, incoming_data):
-    # for event in pygame.event.get():
-    #     if event.type == QUIT:
-    #         quit = True
-    #     if event.type == KEYDOWN:
+        """receives raw data from robot controller and converts into piano note"""
 
-            # quick rescale as a fix
-            note_data = incoming_data
+        # turn previous note off
+        # fluidsynth.stop_Note(note=self.played_note)
 
-            note_data = randrange(13)
-            note_data += 1
+        # which chord
+        # if self.tick:
+        rnd_chord = randrange(3)
+        # if rnd_chord == 0:
+        #     chord = self.dmin7
+        # elif rnd_chord == 1:
+        #     chord = self.g9
+        # else:
+        chord = self.cM7
+        print (chord)
 
-            if note_data == 1:
-                self.play_note(Note("C", self.octave))
-            elif note_data == 2:
-                self.play_note(Note("C#", self.octave))
-            elif note_data == 3:
-                self.play_note(Note("D", self.octave))
-            elif note_data == 4:
-                self.play_note(Note("D#", self.octave))
-            elif note_data == 5:
-                self.play_note(Note("E", self.octave))
-            elif note_data == 6:
-                self.play_note(Note("F", self.octave))
-            elif note_data == 7:
-                self.play_note(Note("F#", self.octave))
-            elif note_data == 8:
-                self.play_note(Note("G", self.octave))
-            elif note_data == 9:
-                self.play_note(Note("G#", self.octave))
-            elif note_data == 10:
-                self.play_note(Note("A", self.octave))
-            elif note_data == 11:
-                self.play_note(Note("A#", self.octave))
-            elif note_data == 12:
-                self.play_note(Note("B", self.octave))
-            elif note_data == 13:
-                self.play_note(Note("C", self.octave + 1))
-            # elif event.key == K_l:
-            #     play_note(Note("C#", octave + 1))
-            # elif event.key == K_PERIOD:
-            #     play_note(Note("D", octave + 1))
-            # elif event.key == K_SEMICOLON:
-            #     play_note(Note("D#", octave + 1))
-            # elif event.key == K_SLASH:
-            #     play_note(Note("E", octave + 1))
-            # elif event.key == K_q:
-            #     play_note(Note("B", octave))
-            # elif event.key == K_w:
-            #     play_note(Note("C", octave + 1))
-            # elif event.key == K_3:
-            #     play_note(Note("C#", octave + 1))
-            # elif event.key == K_e:
-            #     play_note(Note("D", octave + 1))
-            # elif event.key == K_4:
-            #     play_note(Note("D#", octave + 1))
-            # elif event.key == K_r:
-            #     play_note(Note("E", octave + 1))
-            # elif event.key == K_t:
-            #     play_note(Note("F", octave + 1))
-            # elif event.key == K_6:
-            #     play_note(Note("F#", octave + 1))
-            # elif event.key == K_y:
-            #     play_note(Note("G", octave + 1))
-            # elif event.key == K_7:
-            #     play_note(Note("G#", octave + 1))
-            # elif event.key == K_u:
-            #     play_note(Note("A", octave + 1))
-            # elif event.key == K_8:
-            #     play_note(Note("A#", octave + 1))
-            # elif event.key == K_i:
-            #     play_note(Note("B", octave + 1))
-            # elif event.key == K_o:
-            #     play_note(Note("C", octave + 2))
-            # elif event.key == K_0:
-            #     play_note(Note("C#", octave + 2))
-            # elif event.key == K_p:
-            #     play_note(Note("D", octave + 2))
-            # elif event.key == K_MINUS:
-            #     octave -= 1
-            # elif event.key == K_EQUALS:
-            #     octave += 1
-            # elif event.key == K_BACKSPACE:
-            #     channel -= 1
-            # elif event.key == K_BACKSLASH:
-            #     channel += 1
-            # elif event.key == K_ESCAPE:
-            #     quit = True
+        # # which note
+        # len_of_chord = len(chord)
+        # which_note = randrange(len_of_chord)
+        # this_note, this_weight = chord[which_note]
+        # print(this_note, this_weight)
 
-    # Update the screen
-#
-#     pygame.display.update()
-#     tick += 0.001
-# pygame.quit()
+        # rough random for weighting
+        which_weight = random() * 100
+        print(which_weight)
+
+        # chord note or next note?
+        add_whole_note = getrandbits(1)
+        print(add_whole_note)
+
+        current_sum = 0
+        for note_pos, weight in chord:
+            note_name = self.note_list[note_pos + add_whole_note]
+            print(note_name, weight)
+            current_sum += weight
+            if current_sum > which_weight:
+                print('playing', note_name)
+                self.play_note(Note(note_name, self.octave))
+                # self.played_note = note_name
+                break
+
+
+
+
+        #
+        # # work out weighting
+        # # if
+        #
+        #
+        # # quick rescale as a fix
+        # # note_data = incoming_data
+        #
+        #
+        #
+        #
+        # if add_whole_note:
+        #
+        #     note_data = randrange(13)
+        #     note_data += 1
+        #
+        #     if note_data == 1:
+        #         self.play_note(Note("C", self.octave))
+        #     elif note_data == 2:
+        #         self.play_note(Note("C#", self.octave))
+        #     elif note_data == 3:
+        #         self.play_note(Note("D", self.octave))
+        #     elif note_data == 4:
+        #         self.play_note(Note("D#", self.octave))
+        #     elif note_data == 5:
+        #         self.play_note(Note("E", self.octave))
+        #     elif note_data == 6:
+        #         self.play_note(Note("F", self.octave))
+        #     elif note_data == 7:
+        #         self.play_note(Note("F#", self.octave))
+        #     elif note_data == 8:
+        #         self.play_note(Note("G", self.octave))
+        #     elif note_data == 9:
+        #         self.play_note(Note("G#", self.octave))
+        #     elif note_data == 10:
+        #         self.play_note(Note("A", self.octave))
+        #     elif note_data == 11:
+        #         self.play_note(Note("A#", self.octave))
+        #     elif note_data == 12:
+        #         self.play_note(Note("B", self.octave))
+        #     elif note_data == 13:
+        #         self.play_note(Note("C", self.octave + 1))
+
+if __name__ == "__main__":
+    piano = Piano()
+    for x in range(10):
+        piano.which_note(x)
+        sleep(1)
