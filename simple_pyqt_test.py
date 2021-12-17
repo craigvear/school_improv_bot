8f9b46471a7a514d263c34b9105cd560d70c4fd8
craigv <vear2ev2.co.uk>
2021-12-17 11:13:03 +0000

    making it work on Jetbot

--- simple_pyqt_test.py
+++ simple_pyqt_test.py
@@ -1,17 +1,40 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor, QFont
+from PyQt5.QtCore import Qt
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.text = "Лев Николаевич Толстой\nАнна Каренина"
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Drawing text')
+        self.show()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawText(event, qp)
+        qp.end()
+
+    def drawText(self, event, qp):
+        qp.setPen(QColor(168, 34, 3))
+        qp.setFont(QFont('Decorative', 10))
+        qp.drawText(event.rect(), Qt.AlignCenter, self.text)
 
 
 def main():
 
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-
+    ex = Example()
     sys.exit(app.exec_())
 
 
